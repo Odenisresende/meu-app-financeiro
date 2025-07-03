6b538ec6226ac7e5efe513cf42009ad982a2af0a
Odenisresende <odenisresende@gmail.com>
2025-07-03 18:02:22 -0300

    tailwind.config.ts

---
+++ app/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#152638',
+        secondary: '#DDC067', 
+        accent: '#6F4031',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
